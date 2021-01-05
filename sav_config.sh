9d9690b7cdd4e175f27b113dd40dc98a3dcddcc8
Christian Zufferey <christian@zufferey.com>
2021-01-05 20:13:15 +0100

    Terminé le script de sauvegardes de la config d'Home Assistant

--- sav_config.sh
+++ sav_config.sh
@@ -4,7 +4,37 @@
 #pour installer Docker et Docker compose sur une machine Ubuntu c'est ici:
 #https://github.com/zuzu59/docker_demo
 
+echo "
+ATTENTION, il faut arrêter Home Assitant avant avec un 
+
+./stop.sh
+
+Puis après relancer Home Assitant avec un
+
+./start.sh
+
+CTRL-C pour arrêter ou CR pour continuer !
+"
+read
+
 sudo chown -R $USER.$USER ./config
 sudo chmod -R 777 ./config
 
-pas encore terminé !
+YEAR=`date +%Y`
+MONTH=`date +%m`
+DAY=`date +%d`
+#TIME=`date +%H%M%S`
+TIME=`date +%H%M`
+ZTIME=${YEAR}${MONTH}${DAY}.${TIME}
+
+cp -R ./config ./config.$ZTIME.sav
+
+echo "
+Sauvegarde effectuée !
+
+Ne pas oublier de redémarrer Home Assitant avec un 
+
+./start.sh
+
+"
+
